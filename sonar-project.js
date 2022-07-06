da4eba934205ecc7dcf4f0ab898287aa81e1121a
HopeBamiduro <105896680+HopeBamiduro@users.noreply.github.com>
2022-07-06 13:00:21 -0600

    changes made to my sonaquebe token

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login': 'cc1f0546028f17c06a8e576637e78d6b983206f0',
+	    'sonar.login': 'd7390cea846e58a7e2e025c4a180425995a48658',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
